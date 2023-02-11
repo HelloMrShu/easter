592515f4166451a28921c77d19895944a785eaf4
高琪 <gaoqi198@163.com>
2023-02-11 10:08:17 +0800

    feat:add comment

--- global/logger.go
+++ global/logger.go
@@ -25,11 +25,11 @@ func getEncoder() zapcore.Encoder {
 func getLogWriter() zapcore.WriteSyncer {
 	filename := fmt.Sprintf("%s/%s", ServerConfig.Log.Path, ServerConfig.Log.File)
 	lumberJackLogger := &lumberjack.Logger{
-		Filename:   filename,
-		MaxSize:    1,
-		MaxBackups: 5,
-		MaxAge:     30,
-		Compress:   false,
+		Filename:   filename, //日志文件名称
+		MaxSize:    50,       //文件大小限制，默认MB
+		MaxBackups: 5,        //备份文件数量
+		MaxAge:     30,       //最大天数
+		Compress:   false,    //是否压缩
 	}
 	return zapcore.AddSync(lumberJackLogger)
 }
