c99713c62faefdf2233460fca1c1cc89bf31714e
高琪 <gaoqi198@163.com>
2023-02-28 22:58:28 +0800

    feat: add response

--- controller/test/test.go
+++ controller/test/test.go
@@ -1,11 +1,11 @@
 package test
 
 import (
-	. "github.com/HelloMrShu/easter/componets"
+	"github.com/HelloMrShu/easter/componets"
 	"github.com/gin-gonic/gin"
-	"net/http"
 )
 
 func Index(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{"code": ResponseOK, "msg": "hello", "data": nil})
+	res := componets.NewResponse()
+	res.JSON(c)
 }
---
+++ global/constant.go
@@ -0,0 +1,6 @@
+package global
+
+const (
+	ResponseOK  = 0  // 成功返回
+	ResponseErr = -1 //失败返回
+)
